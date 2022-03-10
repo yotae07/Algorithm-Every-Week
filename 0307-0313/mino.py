6faa5c2f7428e7ce4954e492198dafc5e52e974f
minho-lee0716 <minho.lee0716@gmail.com>
2022-03-11 02:51:05 +0900

    0311 - 1문제

--- 0307-0313/mino.py
+++ 0307-0313/mino.py
@@ -1,8 +1,14 @@
-# 2739
-n = int(input())
-for i in range(1, 10):
-    print(f"{n} * {i} = {n * i}")
+# # 2739
+# n = int(input())
+# for i in range(1, 10):
+#     print(f"{n} * {i} = {n * i}")
+#
+# # 2741
+# for i in range(1, int(input()) + 1):
+#     print(i)
 
-# 2741
-for i in range(1, int(input()) + 1):
-    print(i)
+# 2742
+n = int(input())
+while n != 0:
+    print(n)
+    n -= 1
